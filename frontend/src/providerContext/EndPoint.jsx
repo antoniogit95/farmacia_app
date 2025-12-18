6139f3392602bddbb016ff02ccfeef72fa74f968
antoniogit95 <joseantoniovargastellez@gmail.com>
2025-12-18 11:25:51 -0400

    agregar credenciales

--- frontend/src/providerContext/EndPoint.jsx
+++ frontend/src/providerContext/EndPoint.jsx
@@ -1,8 +1,8 @@
 const canbiar = false //true servidor, false local
 
 const LOCAL = "http://localhost:8080";
-const SERVER = "http://192.168.158.54:8080" 
-//const SERVER = "http://10.168.124.54:8080"
+//const SERVER = "http://192.168.158.54:8080" 
+const SERVER = "http://10.168.124.54:8080"
 const endPoint = canbiar? SERVER : LOCAL; 
 
 export const URL_API_public = `${endPoint}/auth`
